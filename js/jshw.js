8ffec53826c9d1395b3d9ceb70713f7fef1b72e1
yuriyyerin <hh88@ukr.net>
2020-07-05 17:56:24 +0300

    new

---
+++ js/jshw.js
@@ -0,0 +1,62 @@
+// let a = "hello"; let b ='owu'; let c='com'; let d='ua'; let number = 1; let numberTwo =10; let numberMinus = -999;
+//  let numberStart= 123; let numberPoint= 3.14; let numberPoint2= 2.7; let numberSixteen= 16; let word= true; let word2= false;
+
+
+
+
+// console.log(a,b,c,d,number,numberTwo,numberMinus,numberMinus,numberStart, numberPoint,numberPoint2,numberSixteen,word,word2);
+// alert (a,b,c);
+// a="privet";
+// alert(a);
+// const wordc = "Yura";
+// const numderc = 36;
+// console.log (wordc);
+// console.log (numderc);
+// const name = prompt ("Enter name");
+// const surname = prompt ("Enter surname");
+// console.log (name, surname);
+// alert(name);
+// alert(surname);
+// let person = name + surname;
+//const Three = +prompt ("Enter number1")
+//const Three2 = +prompt ("Enter number2")
+//const Three3 = +prompt ("Enter number3")
+//console.log(Three, Three2, Three3);
+// const Three = parseInt (prompt ("Enter number1"))
+// const Three2 = parseInt (prompt ("Enter number2"))
+// const Three3 = parseInt (prompt ("Enter number3"))
+// let result = Three+Three2+Three3
+// console.log(result);
+
+// const Three = parseFloat (prompt ("Enter number1"))
+// const Three2 = parseFloat (prompt ("Enter number2"))
+// const Three3 = parseFloat (prompt ("Enter number3"))
+// let result = Three+Three2+Three3
+// console.log(result);
+// let Three = parseFloat (prompt ("Enter number1"))
+// let Three2 = parseFloat (prompt ("Enter number2"))
+// let Three3 = parseFloat (prompt ("Enter number3"))
+// Math.round(Three,Three2,Three3)
+// console.log(Three);
+
+// let result = Three+Three2+Three3
+// console.log(result);
+// let Three = Math.round (prompt ("Enter number1"))
+// let Three2 = Math.round (prompt ("Enter number2"))
+// let result = Math.pow(Three, Three2)
+// console.log (result)
+// let a = 100; let b = '100'; let c = true; let d = undefined;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+// let str = "20";
+//      let a = 5;
+//      document.write(str + a + "<br/>");
+//      document.write(str - a + "<br/>");
+//      document.write(str * "2" + "<br/>");
+//      document.write(str / 2 + "<br/>");
+console.log parseInt("3.14")
+console.log parseInt ("-7.875")
+console.log parseInt("435")
+console.log parseInt("Вася")
